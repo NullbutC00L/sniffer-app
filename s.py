a380668008d7c4feb570be328d1e6a52654c21ea
Fábio Gomes <fg@integrity.pt>
2017-07-27 13:50:53 +0100

    thread creation already working

--- s.py
+++ s.py
@@ -12,6 +12,19 @@ devices=pcapy.findalldevs()
 
 print (devices)
 
+
+##not tested at all
+def VirusTotalApi():
+	print('got here')
+	#params = {'apikey': '-YOUR API KEY HERE-'}
+	#files = {'file': ('myfile.exe', open('myfile.exe', 'rb'))}
+	#response = requests.post('https://www.virustotal.com/vtapi/v2/file/scan', files=files, params=params)
+	json_response = 'a'#response.json()
+
+	f=open('responses.json','a')
+	f.write(json_response)
+	f.close()
+
 def live_capture():
 	cap = pcapy.open_live("any" , 65536 , 1 , 0)
 	dumper= cap.dump_open('scans/'+str(datetime.datetime.now())+'.pcap')
@@ -23,7 +36,7 @@ def live_capture():
 		print(str(header.getcaplen())+'final-->'+str(size))
 	dumper.close()
 	try:
-		thread.start_new_thread(VirusTotalApi)	
+		_thread.start_new_thread(VirusTotalApi,())	
 	except:
 		print('error while thread was going nuts')
 		
@@ -33,22 +46,11 @@ def random_starter():
 
 	print()
 
-	time.sleep(random.randint(10*periodicity,120*periodicity))
+	#time.sleep(random.randint(10*periodicity,120*periodicity))
 	live_capture()
 
 random_starter()
 
-
-##not tested at all
-def VirusTotalApi():
-	params = {'apikey': '-YOUR API KEY HERE-'}
-	files = {'file': ('myfile.exe', open('myfile.exe', 'rb'))}
-	response = requests.post('https://www.virustotal.com/vtapi/v2/file/scan', files=files, params=params)
-	json_response = response.json()
-
-	f=open('responses.json','a')
-	f.write(json_response)
-	f.close()
 
 
 
