1f4fd24a0aa790704666217196940cf473cbbd95
Fábio Gomes <fg@integrity.pt>
2017-07-27 13:08:38 +0100

    defenitive file addded,already creating the scans. VirusTotal connection started

---
+++ s.py
@@ -0,0 +1,57 @@
+import pcapy
+import time	
+import random
+import datetime	
+#Virustotal iimport	
+import requests
+import _thread
+
+
+
+devices=pcapy.findalldevs()
+
+print (devices)
+
+def live_capture():
+	cap = pcapy.open_live("any" , 65536 , 1 , 0)
+	dumper= cap.dump_open('scans/'+str(datetime.datetime.now())+'.pcap')
+	size=0
+	while(size<=120000000):
+		(header,packet)=cap.next()
+		dumper.dump(header,packet)
+		size=size+20+header.getcaplen()
+		print(str(header.getcaplen())+'final-->'+str(size))
+	dumper.close()
+	try:
+		thread.start_new_thread(VirusTotalApi)	
+	except:
+		print('error while thread was going nuts')
+		
+
+def random_starter():
+	periodicity=1
+
+	print()
+
+	time.sleep(random.randint(10*periodicity,120*periodicity))
+	live_capture()
+
+random_starter()
+
+
+##not tested at all
+def VirusTotalApi():
+	params = {'apikey': '-YOUR API KEY HERE-'}
+	files = {'file': ('myfile.exe', open('myfile.exe', 'rb'))}
+	response = requests.post('https://www.virustotal.com/vtapi/v2/file/scan', files=files, params=params)
+	json_response = response.json()
+
+	f=open('responses.json','a')
+	f.write(json_response)
+	f.close()
+
+
+
+
+
+
